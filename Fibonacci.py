fb3492d41fb5f78fee9141e9d55e60ec73e768b8
Oscar CENTENO MORA <oscarcenteno86@gmail.com>
2022-12-06 10:53:20 -0600

    Fibonacci file .py

---
+++ Fibonacci.py
@@ -0,0 +1,62 @@
+#####################################################################
+# Create a function to re create the Fibonacci Series Algorith
+######################################################################
+
+# First, what's Fibonacci Series
+# https://en.wikipedia.org/wiki/Fibonacci_number
+# https://www.mathsisfun.com/numbers/fibonacci-sequence.html
+
+
+# Without a function 
+
+# Program to display the Fibonacci sequence up to n-th term
+
+nterms = int(input("How many terms? "))
+
+# first two terms
+n1, n2 = 0, 1
+count = 0
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+# if there is only one term, return n1
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+# generate fibonacci sequence
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
+       
+       
+# With a Fonction 
+
+
+# Python program to display the Fibonacci sequence
+
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+
+nterms = 11
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))
+
+
+recur_fibo(10)
